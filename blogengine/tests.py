7857ba5bcc378164e024ab281d3822648f260cee
hackerutsav <hackerutsav@yahoo.com>
2014-08-04 22:18:02 +0530

    testing

---
+++ blogengine/tests.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+
+from django.test import TestCase
+from django.utils import timezone
+from blogengine.models import Post
+
+# Create your tests here.
+class PostTest(TestCase):
+	def test_create_post(self):
+		def test_create_post(self):
+			#Create the Post
+			post = Post()
+			
+
